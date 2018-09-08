f06dc2d6e87bd3b869f5cdf7b92370d58d7c28d4
charlottefranklin <charlotte@cfranklin.com>
2018-09-08 12:04:44 +0200

    CF - add SASS and Gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('hello', function() {
+    console.log('Hello Zell');
+});
+
+gulp.task('sass', function(){
+    return gulp.src('assets/scss/**/*.scss')
+        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(gulp.dest('assets/css'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('assets/scss/**/*.scss', ['sass']);
+    // Other watchers
+})
